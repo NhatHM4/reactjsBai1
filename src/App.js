94655bd34107a46b55ae583125ae5140a02f4610
NhatHM4 <haminhnhat711@gmail.com>
2023-01-03 22:18:26 +0700

    react boostrap

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import "./App.scss";
 
 import React from "react";
+import Header from "./components/header/header";
 
 
 const App = () => {
 
   return (
     <div className="app-container">
-      hello world
-      <button className="btn btn-primary">submit</button>
+      <Header/>
     </div>
   );
 };
