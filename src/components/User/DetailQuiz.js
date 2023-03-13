af6e9d741dee386ffc813d56a9721f4a38ecf154
NhatHM4 <haminhnhat711@gmail.com>
2023-03-13 21:35:44 +0700

    update checkbox

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -3,13 +3,14 @@ import { useParams, useLocation } from "react-router-dom";
 import { getQuestionByQuizId, getAllAnswer } from "../../service/apiServices";
 import "./DetaiQuiz.scss";
 import Question from "./Question";
+import _ from "lodash"
 const DetailQuiz = () => {
   const param = useParams();
   const quizid = param.id;
   const location = useLocation();
   const [listQuestion, setListQuestion] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [item, setItem] = useState({})
+  // const [item, setItem] = useState({})
   useEffect(() => {
     sumUpQuestion();
   }, [quizid]);
@@ -20,7 +21,7 @@ const DetailQuiz = () => {
     let arrAns = await getAllAnswer();
     let result = mergeQuestion(arrQues, arrAns);
     setListQuestion(result)
-    setItem(result[currentQuestion]);
+    // setItem(result[currentQuestion]);
   };
 
   const mergeQuestion = (arrQues, arrAns) => {
@@ -30,12 +31,14 @@ const DetailQuiz = () => {
       let answers = [];
       for (let j = 0; j < arrAns.length; j++) {
         if (arrQues[i].id === arrAns[j].questionId) {
+          arrAns[j].isSelected= false
           answers.push(arrAns[j]);
         }
       }
       arr3.push({
         question: arrQues[i],
         answer: answers,
+        
       });
     }
     return arr3;
@@ -46,7 +49,7 @@ const DetailQuiz = () => {
       setCurrentQuestion(listQuestion.length-1)
     } else {
       setCurrentQuestion(currentQuestion+1)
-      setItem(listQuestion[currentQuestion+1]);
+      // setItem(listQuestion[currentQuestion+1]);
     }
   }
 
@@ -55,10 +58,23 @@ const DetailQuiz = () => {
       setCurrentQuestion(0)
     } else {
       setCurrentQuestion(currentQuestion-1)
-      setItem(listQuestion[currentQuestion-1]);
+      // setItem(listQuestion[currentQuestion-1]);
     }
   }
-  console.log(item)
+
+  const handleOnChangeCheckBox = (e,answerId, questionId) =>{
+    let listQuestionClone = _.cloneDeep(listQuestion);
+    // console.log(listQuestion)
+    let item = listQuestionClone.filter(question => +question.question.id === +questionId)
+    if (item) {
+      let answerNho = item[0].answer.filter(answerNho => +answerNho.id === +answerId)
+      if (answerNho){
+        answerNho[0].isSelected = e;
+        setListQuestion(listQuestionClone)
+      }
+    }
+
+  }
   return (
     <div className="detail-quiz-container">
       <div className="left-content">
@@ -66,10 +82,11 @@ const DetailQuiz = () => {
           Quiz-{quizid}: {location?.state?.quizTitle}
           <hr />
         </div>
-        <Question item={item} questionNumber={currentQuestion}/>
+        <Question item={listQuestion ? listQuestion[currentQuestion] :{} } questionNumber={currentQuestion} handleOnChangeCheckBox={handleOnChangeCheckBox}/>
         <div className="footer">
           <button className="btn btn-primary" onClick={()=>{handlePrevious()}}>Prev</button>
           <button className="btn btn-dark" onClick={()=>{handleNext()}}>Next</button>
+          <button className="btn btn-warning" onClick={()=>{handleNext()}}>Finish</button>
         </div>
       </div>
       <div className="right-content">count down</div>
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,40 +1,51 @@
-import { useEffect,useState } from "react";
-const Question = (props)=>{
-    const {item,questionNumber} = props;
-    const [question,setQuestion] = useState(item)
-    useEffect(() => {
-        setQuestion(item)
-    }, [item]);
 
-    return (
-        <>
-        {!(Object.keys(question).length === 0) && 
-            <div >
-            <div className="q-body">
-              <img src={question.question.avatar} />
+const Question = (props) => {
+  const { item, questionNumber,handleOnChangeCheckBox } = props;
+
+  const handleSelected = (e, questionId, answerId)=>{
+    handleOnChangeCheckBox(e.target.checked,questionId, answerId)
+    item.isSelected = e.target.checked
+  }
+  console.log(item)
+  return (
+    <>
+      {!(Object.keys(item).length === 0) && (
+        <div>
+          <div className="q-body">
+            <img src={item.question.avatar} />
+          </div>
+          <div className="q-content">
+            <div className="question">
+              Q-{questionNumber + 1}: {item.question.name}
             </div>
-            <div className="q-content">
-              <div className="question">
-                Q-{questionNumber+1}: {question.question.name}
-              </div>
-              <div className="answer">
-                <div>
-                  {" "}
-                  {question.answer.map((ite, index) => {
-                    return (
-                      <div className="a-chlid" key={`q-${index}`}>
-                        {index}.{ite.answer}
+            <div className="answer">
+              <div>
+                {" "}
+                {item.answer.map((ite, index) => {
+                  return (
+                    <div className="a-chlid" key={`q-${index}`}>
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          onChange={(e)=>{handleSelected(e,ite.id, item.question.id)}}
+                          
+                        />
+                        <label className="form-check-label" >
+                        {ite.answer}
+                        </label>
                       </div>
-                    );
-                  })}
-                </div>
+                      
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
-        }
-             
-        </>
-    )
-}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Question
+export default Question;
